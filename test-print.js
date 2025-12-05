5d1289d17b33f722282c4e0e7f7c55698f1dcd30
Nikola Boricic <nikola@boricic.com>
2025-12-05 10:48:11 -0500

    test print script

--- test-print.js
+++ test-print.js
@@ -1,19 +1,12 @@
 const escpos = require("escpos");
-
-// Enable USB adapter
 escpos.USB = escpos.USB;
 
-// Create USB device (auto-detects TM-T20III)
 const device = new escpos.USB();
-
 const printer = new escpos.Printer(device);
 
 device.open(() => {
   printer
-    .align("ct")
     .text("Hello from Raspberry Pi!")
-    .newline()
-    .text("TM-T20III Test Print OK")
     .newline()
     .cut()
     .close();
