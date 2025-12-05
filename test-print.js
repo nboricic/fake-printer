43e41c1806328d3b3069ddd0a07f94ba82331770
Nikola Boricic <nikola@boricic.com>
2025-12-05 10:50:31 -0500

    test print script

--- test-print.js
+++ test-print.js
@@ -1,12 +1,24 @@
+// test-print.js
 const escpos = require("escpos");
-escpos.USB = escpos.USB;
+// plug in the USB adapter
+const USB = require("escpos-usb");
 
-const device = new escpos.USB();
+// If this throws, escpos-usb isn't installed or its native deps are missing
+const device = new USB(); // you can also pass VID/PID explicitly
+
 const printer = new escpos.Printer(device);
 
-device.open(() => {
+device.open((err) => {
+  if (err) {
+    console.error("Failed to open printer:", err);
+    return;
+  }
+
   printer
+    .align("ct")
     .text("Hello from Raspberry Pi!")
+    .newline()
+    .text("TM-T20III Test Print OK")
     .newline()
     .cut()
     .close();
